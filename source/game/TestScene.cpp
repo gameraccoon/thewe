5a0505fcc80c4b422ebf41635a85b40c3191eb07
Eugene Shcherbakov <shcherbakov.94@inbox.ru>
2014-07-22 16:20:07 +0400

    Движущийся за кликом спрайт

--- source/game/TestScene.cpp
+++ source/game/TestScene.cpp
@@ -1,5 +1,7 @@
 #include "TestsScene.h"
 
+using namespace cocos2d;
+
 bool HelloWorldLayer::init(void)
 {
 	if (!cocos2d::CCLayer::init())
@@ -7,12 +9,27 @@ bool HelloWorldLayer::init(void)
 		return false;
 	}
 
-	cocos2d::CCSize visible_size = cocos2d::CCDirector::sharedDirector()->getVisibleSize();
-	cocos2d::CCPoint origin = cocos2d::CCDirector::sharedDirector()->getVisibleOrigin();
+	_movingSprite = cocos2d::CCSprite::create("../_gamedata/monster.png");
 	
-	cocos2d::CCSprite *sprite = cocos2d::CCSprite::create("../_gamedata/Image.png");
-	sprite->setPosition(ccp(visible_size.width/2.0f + origin.x, visible_size.height/2.0f + origin.y));
-	addChild(sprite);
+	cocos2d::CCRect rect = _movingSprite->getTextureRect();
+	cocos2d::CCPoint pos(0.0f + rect.size.width/2.0f, 0.0f + rect.size.height/2.0f);
+	_movingSprite->setPosition(pos);
+	_movingSprite->setScale(2.5f);
+
+	_touchPos = pos;
+
+	CCLayer::addChild(_movingSprite);
+	CCLayer::setTouchEnabled(true);
+	CCLayer::schedule(schedule_selector(HelloWorldLayer::_IdleUpdate));
+
+	//CCLayer::ccTouchMoved
+
+	//cocos2d::CCSize visible_size = cocos2d::CCDirector::sharedDirector()->getVisibleSize();
+	//cocos2d::CCPoint origin = cocos2d::CCDirector::sharedDirector()->getVisibleOrigin();
+	
+	//cocos2d::CCSprite *sprite = cocos2d::CCSprite::create("../_gamedata/Image.png");
+	//sprite->setPosition(ccp(visible_size.width/2.0f + origin.x, visible_size.height/2.0f + origin.y));
+	//addChild(sprite);
 
 	return true;
 }
@@ -30,5 +47,38 @@ cocos2d::CCScene* HelloWorldLayer::scene(void)
 
 void  HelloWorldLayer::menuCloseCallback(cocos2d::CCObject *Sender)
 {
+}
 
+void HelloWorldLayer::ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event)
+{
+	cocos2d::CCTouch *touch = (cocos2d::CCTouch *)touches->anyObject();
+	_touchPos = touch->getLocation();
+
+	//const float duration = 0.4f;
+	//cocos2d::CCMoveTo *move = cocos2d::CCMoveTo::create(duration, _touchPos);
+	//_movingSprite->runAction(move);
+}
+
+void HelloWorldLayer::ccTouchMoved(cocos2d::CCTouch *touch, cocos2d::CCEvent *event)
+{
+	_touchPos = touch->getLocation();
+}
+
+void HelloWorldLayer::_IdleUpdate(float timeDelta)
+{
+	cocos2d::CCPoint pos = _movingSprite->getPosition();
+
+	cocos2d::CCPoint dir = _touchPos - pos;
+	
+	if (dir.getLength() >= 5.0f)
+	{
+		dir = dir.normalize();
+
+		float velocity = 500.0f;
+	
+		pos.x += dir.x * velocity * timeDelta;
+		pos.y += dir.y * velocity * timeDelta;
+
+		_movingSprite->setPosition(pos);
+	}
 }
--- source/game/TestsScene.h
+++ source/game/TestsScene.h
@@ -13,6 +13,15 @@ public:
 	void menuCloseCallback(cocos2d::CCObject *Sender);
 
 	CREATE_FUNC(HelloWorldLayer);
+
+	void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
+	void ccTouchMoved(cocos2d::CCTouch *touch, cocos2d::CCEvent *event);
+
+private:
+	cocos2d::CCSprite *_movingSprite;
+	cocos2d::CCPoint _touchPos;
+
+	void _IdleUpdate(float timeDelta);
 };
 
 #endif
