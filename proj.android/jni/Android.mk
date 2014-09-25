454aaba1c08dbe315db7d1c7157d440e65725560
Pavel Grebnev <mail@ruwhynot.com>
2014-09-26 00:18:34 +0400

    Компиляция на android с Luabind

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -27,6 +27,8 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 				   	../../Classes/FileUtils.cpp \
 				   	../../Classes/GameScene.cpp \
 				   	../../Classes/Log.cpp \
+				   	../../Classes/LuaBindings.cpp \
+				   	../../Classes/LuaInstance.cpp \
 				   	../../Classes/MessageManager.cpp \
 				   	../../Classes/MessageWidget.cpp \
 				   	../../Classes/MainMenuScene.cpp \
@@ -47,12 +49,61 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 				   	../../Classes/World.cpp \
 				   	../../Classes/WorldLoader.cpp \
 				   	../../Classes/WorldMapLayer.cpp \
-                  	../../dependencies/pugixml-1.4/pugixml.cpp
+                  	../../dependencies/pugixml-1.4/pugixml.cpp \
+                  	\
+                  	../../dependencies/luabind-0.9.1/src/class.cpp \
+					../../dependencies/luabind-0.9.1/src/class_info.cpp \
+					../../dependencies/luabind-0.9.1/src/class_registry.cpp \
+					../../dependencies/luabind-0.9.1/src/class_rep.cpp \
+					../../dependencies/luabind-0.9.1/src/create_class.cpp \
+					../../dependencies/luabind-0.9.1/src/error.cpp \
+					../../dependencies/luabind-0.9.1/src/exception_handler.cpp \
+					../../dependencies/luabind-0.9.1/src/function.cpp \
+					../../dependencies/luabind-0.9.1/src/inheritance.cpp \
+					../../dependencies/luabind-0.9.1/src/link_compatibility.cpp \
+					../../dependencies/luabind-0.9.1/src/object_rep.cpp \
+					../../dependencies/luabind-0.9.1/src/open.cpp \
+					../../dependencies/luabind-0.9.1/src/pcall.cpp \
+					../../dependencies/luabind-0.9.1/src/scope.cpp \
+					../../dependencies/luabind-0.9.1/src/stack_content_by_name.cpp \
+					../../dependencies/luabind-0.9.1/src/weak_ref.cpp \
+					../../dependencies/luabind-0.9.1/src/wrapper_base.cpp \
+					\
+					../../dependencies/lua-5.1.5/lapi.c \
+					../../dependencies/lua-5.1.5/lauxlib.c \
+					../../dependencies/lua-5.1.5/lbaselib.c \
+					../../dependencies/lua-5.1.5/lcode.c \
+					../../dependencies/lua-5.1.5/ldblib.c \
+					../../dependencies/lua-5.1.5/ldebug.c \
+					../../dependencies/lua-5.1.5/ldo.c \
+					../../dependencies/lua-5.1.5/ldump.c \
+					../../dependencies/lua-5.1.5/lfunc.c \
+					../../dependencies/lua-5.1.5/lgc.c \
+					../../dependencies/lua-5.1.5/linit.c \
+					../../dependencies/lua-5.1.5/liolib.c \
+					../../dependencies/lua-5.1.5/llex.c \
+					../../dependencies/lua-5.1.5/lmathlib.c \
+					../../dependencies/lua-5.1.5/lmem.c \
+					../../dependencies/lua-5.1.5/loadlib.c \
+					../../dependencies/lua-5.1.5/lobject.c \
+					../../dependencies/lua-5.1.5/lopcodes.c \
+					../../dependencies/lua-5.1.5/loslib.c \
+					../../dependencies/lua-5.1.5/lparser.c \
+					../../dependencies/lua-5.1.5/lstate.c \
+					../../dependencies/lua-5.1.5/lstring.c \
+					../../dependencies/lua-5.1.5/lstrlib.c \
+					../../dependencies/lua-5.1.5/ltable.c \
+					../../dependencies/lua-5.1.5/ltablib.c \
+					../../dependencies/lua-5.1.5/ltm.c \
+					../../dependencies/lua-5.1.5/lundump.c \
+					../../dependencies/lua-5.1.5/lvm.c \
+					../../dependencies/lua-5.1.5/lzio.c \
+					../../dependencies/lua-5.1.5/print.c
                    
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
-					$(LOCAL_PATH)/../../dependencies/boost_1_56_0 \
-					$(LOCAL_PATH)/../../dependencies/lua-5.1 \
+					$(LOCAL_PATH)/../../dependencies/boost-1.55.0 \
+					$(LOCAL_PATH)/../../dependencies/lua-5.1.5 \
 					$(LOCAL_PATH)/../../dependencies/luabind-0.9.1 \
 					$(LOCAL_PATH)/../../dependencies/pugixml-1.4 \
 					$(LOCAL_PATH)/../../cocos2d/extensions
--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := c++_static
-NDK_TOOLCHAIN_VERSION=clang
+NDK_TOOLCHAIN_VERSION=4.8
 
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
