a8c6184fa21fac2362966aaae0b40bdc72140663
Pavel Grebnev <mail@ruwhynot.com>
2014-11-03 15:24:29 +0300

    Правка к предыдущим трём коммитам!

--- Classes/MapProjector.cpp
+++ Classes/MapProjector.cpp
@@ -24,9 +24,9 @@ void MapProjector::SetScale(float scale)
 	{
 		// align view to smaller border
 #ifndef WIN32
-		_viewScale = 2 * ((_screenCenter.y / _mapSize.y) > (_screenCenter.x / _mapSize.x) ? (_screenCenter.y / _mapSize.y) : (_screenCenter.x / _mapSize.x));
+		_viewScale = 2 * std::max(_screenCenter.y / _mapSize.y, _screenCenter.x / _mapSize.x);
 #else
-		_viewScale = 2 * max(_screenCenter.y / _mapSize.y, _screenCenter.x / _mapSize.x);
+		_viewScale = 2 * ((_screenCenter.y / _mapSize.y) > (_screenCenter.x / _mapSize.x) ? (_screenCenter.y / _mapSize.y) : (_screenCenter.x / _mapSize.x));
 #endif
 	}
 	else
