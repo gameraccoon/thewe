7d5aedff83e7049572e49439f3a3ff2648e15b51
Pavel Grebnev <mail@ruwhynot.com>
2014-11-29 17:47:57 +0300

    Скрипт экспорта локализаций фиксит проблему с двойными переносами строки

--- utils/UpdateText.py
+++ utils/UpdateText.py
@@ -1,5 +1,20 @@
 from zipfile import ZipFile
 
+def fix_issues(fileForFix):
+	content = []
+	with open(fileForFix) as f:
+		content = f.read()
+
+	# fix double line breaks issue
+	content = content.replace('<text:p/>', '<text:p></text:p>')
+
+	with open(fileForFix, 'w') as f:
+		f.write(content)
+
+# unpacking localizations
 with open('../Resources/texts/content.xml', 'w') as f:
 	with ZipFile('../UISources/Localizations.ods', 'r') as z:
 		f.write(z.read('content.xml'))
+
+# fix some issues
+fix_issues("../Resources/texts/content.xml")
